a9c94b3b0e30a672c0ec432002c920fd095705fe
perrada69 <bursa@kchpbt.cz>
2020-05-05 12:54:23 +0200

    CleanUp

--- compile.bat
+++
@@ -1,3 +0,0 @@
-sjasmplus.exe cc.asm --lst=cc.lst --sym=cc.sym.txt
-e:\cspect\hdfmonkey.exe put e:\cspect\cspect-next-2gb.img CalmCommander.nex CalmCommander/
-e:\cspect\CSpect.exe -zxnext -basickeys -nextrom -map=player.map -tv -mmc=e:\cspect\cspect-next-2gb.img
