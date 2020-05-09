522ab77f7738b565d2769ef1e83dacc2362e2c60
perrada69 <bursa@kchpbt.cz>
2020-05-09 16:04:19 +0200

    Změna verze na 0.2

--- functions/menu.asm
+++ functions/menu.asm
@@ -279,7 +279,7 @@ menuenter
             jp (hl)
 
 text 		defb " LEFT  | FILE  | UTILS | RIGHT | QUIT                                                       ",0
-nadpis 		defb " Calm Commander 0.1 (Development version)                                                   ",0
+nadpis 		defb " Calm Commander 0.2 (Development version)                                                   ",0
 menupos		defb 0, 18, 34, 50,66
 
 nummenu		defb 0				;jakou položku zobrazit v menu
